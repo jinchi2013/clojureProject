b9dc12a60f162deced0926bd8634f3e743f63c7f
Calvin Jin <Calvin.Jin.CFT@jcrew.com>
2016-10-31 17:14:58 -0400

    queue(enqueue) like behavior by future promise delay

---
+++ rolling-your-own-queue-ch9/test/rolling_your_own_queue_ch9/core_test.clj
@@ -0,0 +1,7 @@
+(ns rolling-your-own-queue-ch9.core-test
+  (:require [clojure.test :refer :all]
+            [rolling-your-own-queue-ch9.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
